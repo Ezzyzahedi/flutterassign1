81734573bd25662521062cb4af3d20ddbc6e16f1
Sylvester <sylvester.das@gmail.com>
2019-02-24 23:36:16 +0530

    Google maps integration and navigation complete

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,11 +4,13 @@ class Constants {
   static const INSTA_ICON_URL = "https://cdn4.iconfinder.com/" +
       "data/icons/social-messaging-ui-color-shapes-2-free/" +
       "128/social-instagram-new-circle-512.png";
-  static var INSTA_REDIRECT_URI = Uri.encodeFull("http://flutter-assign-1.firebaseapp.com");
+  static var instaRedirectUri = Uri.encodeFull("http://flutter-assign-1.firebaseapp.com");
 
   static const FB_APP_ID = '630122220771748';
   static const FB_APP_SECRET = '3760b5e57babd252e08bd408f3258177';
   static const FB_ICON_URL = "https://cdn4.iconfinder.com/" +
       "data/icons/social-media-icons-the-circle-set/48/" +
       "facebook_circle-512.png";
+
+  static const GOOGLE_API_KEY = 'AIzaSyDHQkYWxVN4eWVffGjdean1cyCP8wN0zbs';
 }
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_assign1/constants.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:location/location.dart' as LocationManager;
+import 'package:url_launcher/url_launcher.dart';
+
+
+GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: Constants.GOOGLE_API_KEY);
 
 class Welcome extends StatelessWidget {
   @override
@@ -17,7 +25,44 @@ class _WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<_WelcomePage> {
+
+  GoogleMapController mapController;
+  List<PlacesSearchResult> places = [];
+
   var displayName = '';
+
+  void _onMapCreated(GoogleMapController controller, context) {
+    setState(() {
+      mapController = controller;
+      _getLocation(context);
+    });
+  }
+
+  _getLocation(context) async {
+
+    new LocationManager.Location().getLocation().then((loc) {
+      double lat, lng;
+      LatLng myLocation;
+      loc.forEach((k, v) {
+        if (k.startsWith("latitude")) lat = v;
+        if (k.startsWith("longitude")) lng = v;
+      });
+
+      print('lat: ' + lat.toString() + '; lng: ' + lng.toString());
+      myLocation = LatLng(lat, lng);
+      mapController.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+          bearing: 270.0,
+          target: myLocation,
+          tilt: 30.0,
+          zoom: 16.0,
+        ),
+      ));
+
+      getNearbyPlaces(myLocation, context);
+    }).catchError((err) => print('err: ' + err));
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -34,19 +79,81 @@ class _WelcomePageState extends State<_WelcomePage> {
           "Welcome ${this.displayName}"
         ),
       ),
-      body: Container(
-        color: Colors.white,
-        alignment: Alignment.center,
-        child: MaterialButton(
-          color: Colors.orangeAccent,
-          elevation: 2,
-          onPressed: () {
-            FirebaseAuth.instance.signOut();
-            Navigator.of(context).pushReplacementNamed('/');
-          },
-          child: Text("Sign out"),
-        ),
-      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: GoogleMap(
+              onMapCreated: (controller) {
+                _onMapCreated(controller, context);
+              },
+              options: GoogleMapOptions(
+                myLocationEnabled: true,
+                compassEnabled: true,
+                zoomGesturesEnabled: true,
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
+
+  void getNearbyPlaces(LatLng center, context) async {
+
+    final location = Location(center.latitude, center.longitude);
+    final result = await _places.searchNearbyWithRadius(
+      location, 1500, keyword: "axis bank"
+    );
+    setState(() {
+      if (result.status == "OK") {
+        this.places = result.results;
+        LatLng place;
+        result.results.getRange(0, 1).forEach((f) {
+          place = LatLng(f.geometry.location.lat, f.geometry.location.lng);
+          final markerOptions = MarkerOptions(
+            position: place,
+            infoWindowText: InfoWindowText(
+              "${f.name}", "${f.types?.first}"));
+          mapController.addMarker(markerOptions);
+        });
+
+        _showDialog(place, context);
+      }
+    });
+  }
+
+  _launchURL(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  _showDialog(place, context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Navigate?"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            new FlatButton(
+              child: new Text("Navigate"),
+              onPressed: () {
+                _launchURL("google.navigation:q=${place.latitude},${place.longitude}");
+              },
+            ),
+          ],
+        );
+      }
+    );
+  }
+
 }
